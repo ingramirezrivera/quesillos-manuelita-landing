607c903b8830442250094c90c4c89206b047798e
ingramirezrivera <ing.ramirezrivera@gmail.com>
2025-08-05 01:33:32 -0700

    Version inicial del landing quesillos manuelita

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fbc02d",
+        secondary: "#00a8e8",
+        accent: "#34a853",
+      },
+    },
+  },
+  plugins: [],
+};
