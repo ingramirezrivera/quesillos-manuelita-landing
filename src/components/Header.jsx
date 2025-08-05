55940ca6680cc2bd5937c6f046d6204f9b039348
ingramirezrivera <ing.ramirezrivera@gmail.com>
2025-08-05 15:56:40 -0700

    feat: Primer header basico

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,40 @@
+import logo from '../assets/logo.jpeg';
+
 export default function Header() {
   return (
-    <header className="bg-white shadow">
-      <div className="max-w-6xl mx-auto flex items-center justify-between px-4 py-3">
-        <nav>
-          <ul className="flex gap-6 text-gray-700 font-medium">
-            <li><a href="#hero" className="hover:text-primary">Inicio</a></li>
-            <li><a href="#about" className="hover:text-primary">Sobre nosotros</a></li>
-            <li><a href="#products" className="hover:text-primary">Productos</a></li>
-            <li><a href="#contact" className="hover:text-primary">Contacto</a></li>
+    <header className="bg-white shadow-md sticky top-0 z-20 realtive rounded-b-3xl bg-white">
+      <div className="max-w-7xl mx-auto flex items-center px-4 py-2 h-16">
+        {/* Logo a la izquierda con una sombra y bordes redondeados */}
+        <img
+          src={logo}
+          alt="Quesillos Manuelita"
+          className="hover:scale-105 transition-transform duration-200 h-28 mt-20 w-auto -mb-4 rounded-xl drop-shadow-2xl mr-6 flex-shrink-0"
+        />
+
+        {/* Menú de navegación alineado a la derecha */}
+        <nav className="ml-auto">
+          <ul className="flex gap-8 text-gray-700 font-medium">
+            <li>
+              <a href="#hero" className="inline-block transform-gpu transition-transform duration-200 hover:scale-110 focus:scale-115 hover:drop-shadow-[0_2px_2px_rgba(0,0,0,0.1)] ">
+                Inicio
+              </a>
+            </li>
+            <li>
+              <a href="#products" className="inline-block transform-gpu transition-transform duration-200 hover:scale-110 focus:scale-115 hover:drop-shadow-[0_2px_2px_rgba(0,0,0,0.1)] ">
+                Productos
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="inline-block transform-gpu transition-transform duration-200 hover:scale-110 focus:scale-115 hover:drop-shadow-[0_2px_2px_rgba(0,0,0,0.1)] ">
+                Sobre nosotros
+              </a>
+            </li>
+            
+            <li>
+              <a href="#contact" className="inline-block transform-gpu transition-transform duration-200 hover:scale-110 focus:scale-115 hover:drop-shadow-[0_2px_2px_rgba(0,0,0,0.1)]  ">
+                Contacto
+              </a>
+            </li>
           </ul>
         </nav>
       </div>
