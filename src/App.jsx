ef40ca0977c72ade0db9708c06ba7a8ef1035eac
ingramirezrivera <ing.ramirezrivera@gmail.com>
2025-08-06 15:50:19 -0700

    Footer listo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Header from "./components/Header";
 import Hero from "./features/hero/Hero";
 import Products from "./features/products/Products";
 import About from "./features/about/About";
-import Contact from "./features/contact/Contact";
+import Contact from "./features/contact/contact";
+import Footer from "./features/footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Products />
       <About />
       <Contact />
+      <Footer />
     </>
   );
 }
